c4afcac2962a4d20a6f921398c681d3d754c559c
Daniel K. Guolo <daniel_kv@hotmail.com>
2025-02-06 18:59:38 -0300

    rebase

--- src/modules/experience/experience.service.ts
+++ src/modules/experience/experience.service.ts
@@ -8,30 +8,27 @@ import { ExperiencePayload } from './experience.interface'
 export class ExperienceService {
     constructor(private prisma: PrismaService) {}
 
-    async createExperience(experienceData: ExperiencePayload) {
+    createExperience(experienceData: ExperiencePayload) {
         const copy = experienceData.medias
         experienceData.medias = { create: [{ media: copy }] }
 
         return this.prisma.experience.create({ data: experienceData })
     }
 
-    async getExperience(experienceID: number) {
+    getExperience(experienceID: number) {
         return this.prisma.experience.findUnique({
             where: { id: experienceID },
         })
     }
 
-    async updateExperience(
-        experienceData: ExperiencePayload,
-        experienceID: number,
-    ) {
+    updateExperience(experienceData: ExperiencePayload, experienceID: number) {
         return this.prisma.experience.update({
             where: { id: experienceID },
             data: experienceData,
         })
     }
 
-    async deleteExperience(experienceID: number) {
+    deleteExperience(experienceID: number) {
         return this.prisma.experience.delete({ where: { id: experienceID } })
     }
 }
