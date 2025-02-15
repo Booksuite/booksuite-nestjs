5df76900a38054f33c91b240c3a1b29db1473b9d
Daniel K. Guolo <daniel_kv@hotmail.com>
2025-02-15 17:39:58 -0300

    fix setup

--- src/modules/company/company.module.ts
+++ src/modules/company/company.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common'
 
 import { CompanyController } from './company.controller'
 import { CompanyService } from './company.service'
-import { CompanyContactService } from './companyCategory.service'
+import { CompanyContactService } from './companyContact.service'
 
 @Module({
     providers: [CompanyService, CompanyContactService],
