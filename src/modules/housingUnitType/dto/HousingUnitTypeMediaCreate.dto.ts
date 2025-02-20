af668b7ed88a2798ba81ae91188bbc07cd583b59
Zayma1 <zaymakinsiona@gmail.com>
2025-02-20 15:48:53 -0300

    refactored DTOS for swagger recognize

--- src/modules/housingUnitType/dto/HousingUnitTypeMediaCreate.dto.ts
+++ src/modules/housingUnitType/dto/HousingUnitTypeMediaCreate.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger'
 import {
     IsBoolean,
     IsDefined,
@@ -7,18 +8,25 @@ import {
 } from 'class-validator'
 
 export class HousingUnitTypeMediaCreateDTO {
+    @ApiProperty({
+        example: 'true',
+        description: 'It is part of one of the best features',
+    })
     @IsDefined()
     @IsBoolean()
     isFeatured!: boolean
 
+    @ApiProperty({ example: '1' })
     @IsOptional()
     @IsInt()
     order?: number
 
+    @ApiProperty({ example: 'f47ac10b-58cc-4372-a567-0e02b2c3d479' })
     @IsDefined()
     @IsString()
     propertyId!: string
 
+    @ApiProperty({ example: '6ba7b810-9dad-11d1-80b4-00c04fd430c8' })
     @IsDefined()
     @IsString()
     mediaId!: string
