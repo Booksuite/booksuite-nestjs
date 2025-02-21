ba115521977910ff47238c90ec9f26a80b17b6fa
Daniel K. Guolo <daniel_kv@hotmail.com>
2025-02-21 15:38:01 -0300

    Zayma/refactoring rest dtos 1 (#37)
    * refactored DTOS for swagger recognize
    
    * requested fixes
    
    * fixes
    
    * refactored DTOS for swagger recognize
    
    * requested fixes
    
    * fixes
    
    ---------
    
    Co-authored-by: Zayma1 <zaymakinsiona@gmail.com>

--- src/modules/service/dtos/ServiceCreate.dto.ts
+++ src/modules/service/dtos/ServiceCreate.dto.ts
@@ -1,9 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { Type } from 'class-transformer'
 import {
     IsArray,
     IsBoolean,
     IsDefined,
     IsInt,
+    IsISO8601,
     IsOptional,
     IsString,
     ValidateIf,
@@ -14,48 +16,59 @@ import { ServiceCategoryCreateDTO } from './ServiceCategoryCreate.dto'
 import { ServiceMediaCreateDTO } from './ServiceMediaCreate.dto'
 
 export class ServiceCreateDTO {
+    @ApiProperty({ example: 'massage' })
     @IsDefined()
     @IsString()
     name!: string
 
+    @ApiProperty({ example: 'PER_PERSON' })
     @IsDefined()
     @IsString()
     billType!: string
 
+    @ApiProperty({ example: 200 })
     @IsDefined()
     price!: number
 
+    @ApiProperty({ example: 3 })
     @IsDefined()
     @IsInt()
     adults!: number
 
+    @ApiProperty({ example: 1 })
     @IsDefined()
     @IsInt()
     minDaily!: number
 
+    @ApiProperty({ example: 1 })
     @IsDefined()
     @IsInt()
     minNotice!: number
 
+    @ApiProperty({ example: true })
     @IsDefined()
     @IsBoolean()
     onlineSale!: boolean
 
+    @ApiProperty({ example: false })
     @IsDefined()
     @IsBoolean()
     panelSale!: boolean
 
+    @ApiProperty({ example: true })
     @IsDefined()
     @IsBoolean()
     seasonalSale!: boolean
 
+    @ApiProperty({ example: '2025-02-21T14:30:00.000Z' })
     @IsDefined()
-    @IsString()
-    seasonStart!: Date
+    @IsISO8601()
+    seasonStart!: string
 
+    @ApiProperty({ example: '2025-02-22T14:30:00.000Z' })
     @IsDefined()
-    @IsString()
-    seasonEnd!: Date
+    @IsISO8601()
+    seasonEnd!: string
 
     // @IsOptional()
     // hosting?: Prisma.JsonValue
@@ -63,33 +76,42 @@ export class ServiceCreateDTO {
     // @IsOptional()
     // nights?: Prisma.JsonValue
 
+    @ApiProperty({
+        example: 'Enjoy a soothing massage during your stay',
+    })
     @IsDefined()
     @IsString()
     description!: string
 
+    @ApiProperty({ example: 'Free Wi-Fi, Breakfast, Swimming Pool Access' })
     @IsDefined()
     @IsString()
     included!: string
 
+    @ApiProperty({ example: 'Seasonal availability, blackout dates apply.' })
     @IsDefined()
     @IsString()
     notes!: string
 
+    @ApiProperty({ example: 'https://www.example.com/video', required: false })
     @IsOptional()
     @IsString()
     videoUrl?: string
 
+    @ApiProperty({ type: [ServiceMediaCreateDTO] })
     @IsDefined()
     @IsArray()
     @Type(() => ServiceMediaCreateDTO)
     @ValidateNested({ each: true })
     medias!: ServiceMediaCreateDTO[]
 
+    @ApiProperty({ example: '3c39db8f-ec01-4bf1-89e3-a4b458903b2c' })
     @ValidateIf((o) => !o.category)
     @IsDefined()
     @IsString()
     categoryId?: string
 
+    @ApiProperty({ type: [ServiceCategoryCreateDTO] })
     @ValidateIf((o) => !o.categoryId)
     @IsDefined()
     @ValidateNested()
--- src/modules/service/dtos/ServiceSearchQuery.dto.ts
+++ src/modules/service/dtos/ServiceSearchQuery.dto.ts
@@ -1,35 +1,44 @@
-import { IsDefined, IsString } from 'class-validator'
+import { ApiProperty } from '@nestjs/swagger'
+import { IsDefined, IsISO8601, IsString } from 'class-validator'
 
 export class ServiceSearchQueryDTO {
+    @ApiProperty({ example: 'deluxe suite' })
     @IsDefined()
     @IsString()
     name!: string
 
+    @ApiProperty({ example: 350 })
     @IsDefined()
-    price!: string
+    price!: number
 
+    @ApiProperty({ example: 2 })
     @IsDefined()
-    adults!: string
+    adults!: number
 
+    @ApiProperty({ example: 1 })
     @IsDefined()
-    minDaily!: string
+    minDaily!: number
 
+    @ApiProperty({ example: 1 })
     @IsDefined()
-    minNotice!: string
+    minNotice!: number
 
+    @ApiProperty({ example: '2025-02-22T14:30:00.000Z' })
+    @IsDefined()
+    @IsISO8601()
+    seasonStart!: string
+
+    @ApiProperty({ example: '2025-02-23T14:30:00.000Z' })
+    @IsDefined()
+    @IsISO8601()
+    seasonEnd!: string
+
+    @ApiProperty({ example: 1 })
+    @IsDefined()
+    page: number
+
+    @ApiProperty({ example: 5 })
     @IsDefined()
     @IsString()
-    seasonStart!: Date
-
-    @IsDefined()
-    @IsString()
-    seasonEnd!: Date
-
-    @IsDefined()
-    @IsString()
-    page: string
-
-    @IsDefined()
-    @IsString()
-    itemsPerPage: string
+    itemsPerPage: number
 }
