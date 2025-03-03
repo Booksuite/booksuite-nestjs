fb25c3eab51388591e5976277d516687184aad2e
Daniel K. Guolo <daniel_kv@hotmail.com>
2025-03-03 10:27:22 -0300

    fix dto

--- src/modules/company/dto/AgeGroupResponse.dto.ts
+++ src/modules/company/dto/AgeGroupResponse.dto.ts
@@ -1,33 +1,22 @@
 import { ApiProperty } from '@nestjs/swagger'
 import { AgeGroupChargeType } from '@prisma/client'
-import { IsDefined, IsInt, IsNumber, IsOptional } from 'class-validator'
 
 export class AgeGroupResponseDTO {
-    @ApiProperty({
-        example: 'e95517fb-b3bb-492f-b605-d289704cde0e',
-        required: false,
-    })
-    id?: string
+    @ApiProperty({ example: 'e95517fb-b3bb-492f-b605-d289704cde0e' })
+    id: string
 
     @ApiProperty({ example: 0 })
-    @IsDefined()
-    @IsInt()
     initialAge!: number
 
     @ApiProperty({ example: 12 })
-    @IsDefined()
-    @IsInt()
     finalAge!: number
 
     @ApiProperty({
         enum: AgeGroupChargeType,
         example: AgeGroupChargeType.DAILY_PERCENTAGE_PER_CHILDREN,
     })
-    @IsDefined()
     chargeType!: AgeGroupChargeType
 
     @ApiProperty({ example: 50, required: false })
-    @IsOptional()
-    @IsNumber()
     value?: number | null
 }
