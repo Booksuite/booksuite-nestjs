2d6099c0f3d8e1b925840fb4d64683f858ab5d07
Zaymã Kinsiona <127459308+Zayma1@users.noreply.github.com>
2025-03-03 09:47:37 -0300

    added responses DTOs for facility module (#43)
    * added responses DTOs for facility module
    
    * request fixes

--- src/modules/facility/facility.controller.ts
+++ src/modules/facility/facility.controller.ts
@@ -7,26 +7,46 @@ import {
     Patch,
     Post,
 } from '@nestjs/common'
+import { ApiExtraModels, ApiOkResponse, getSchemaPath } from '@nestjs/swagger'
 
 import { FacilityDTO } from './dto/Facility.dto'
+import { FacilityResponseDTO } from './dto/FacilityResponse.dto'
 import { FacilityService } from './facility.service'
 
+@ApiExtraModels(FacilityResponseDTO)
 @Controller('facility')
 export class FacilityController {
     constructor(private facilityService: FacilityService) {}
 
+    @ApiOkResponse({ type: FacilityResponseDTO })
     @Post('create')
-    create(@Body() facilityData: FacilityDTO) {
+    create(@Body() facilityData: FacilityDTO): Promise<FacilityResponseDTO> {
         return this.facilityService.create(facilityData)
     }
 
+    @ApiOkResponse({
+        schema: {
+            oneOf: [
+                {
+                    $ref: getSchemaPath(FacilityResponseDTO),
+                },
+                { type: 'null' },
+            ],
+        },
+    })
     @Get(':id')
-    getById(@Param('id') facilityId: string) {
+    getById(
+        @Param('id') facilityId: string,
+    ): Promise<FacilityResponseDTO | null> {
         return this.facilityService.getById(facilityId)
     }
 
+    @ApiOkResponse({ type: FacilityResponseDTO })
     @Patch(':id')
-    update(@Param('id') facilityId: string, facilityData: FacilityDTO) {
+    update(
+        @Param('id') facilityId: string,
+        facilityData: FacilityDTO,
+    ): Promise<FacilityResponseDTO> {
         return this.facilityService.update(facilityId, facilityData)
     }
 
