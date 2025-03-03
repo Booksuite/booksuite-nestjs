59748d5ae8ecd2367761ac346b6cd726655d2c96
Zayma1 <zaymakinsiona@gmail.com>
2025-03-03 15:23:29 -0300

    fix reservationService creation

--- src/modules/reservation/dto/ReservationCreate.dto.ts
+++ src/modules/reservation/dto/ReservationCreate.dto.ts
@@ -60,5 +60,5 @@ export class ReservationCreateDTO {
     @IsDefined()
     @IsArray()
     @Type(() => ReservationServiceCreateDTO)
-    services!: ReservationServiceCreateDTO
+    services!: ReservationServiceCreateDTO[]
 }
