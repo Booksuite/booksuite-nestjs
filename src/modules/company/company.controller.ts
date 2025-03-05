dadfaa67415440f17657372055cd7cb41db52c6c
Zayma1 <zaymakinsiona@gmail.com>
2025-03-05 14:58:31 -0300

    Added search endpoint to company controller

--- src/modules/company/company.controller.ts
+++ src/modules/company/company.controller.ts
@@ -6,13 +6,19 @@ import {
     Param,
     Patch,
     Post,
+    Query,
 } from '@nestjs/common'
 import { ApiExtraModels, ApiOkResponse, getSchemaPath } from '@nestjs/swagger'
 
+import { PaginationQuery } from '@/common/types/pagination'
+
 import { CompanyService } from './company.service'
 import { CompanyCreateDTO } from './dto/CompanyCreate.dto'
+import { CompanyOrderByDTO } from './dto/CompanyOrderBy.dto'
+import { CompanyQueryDTO } from './dto/CompanyQuery.dto'
 import { CompanyResponseDTO } from './dto/CompanyResponse.dto'
 import { CompanyResponseFullDTO } from './dto/CompanyResponseFull.dto'
+import { CompanyResponsePaginatedDTO } from './dto/CompanyResponsePaginated.dto'
 
 @ApiExtraModels(CompanyResponseFullDTO)
 @Controller('company')
@@ -51,6 +57,14 @@ export class CompanyController {
         return this.companyService.update(id, updatedData)
     }
 
+    @Post('search')
+    search(
+        @Body() pagination: PaginationQuery,
+        @Body() order: CompanyOrderByDTO,
+        @Query() queryParams: CompanyQueryDTO,
+    ): Promise<CompanyResponsePaginatedDTO> {
+        return this.companyService.search(pagination, order, queryParams)
+    }
     @Delete(':id')
     deleteCompany(@Param('id') id: string) {
         return this.companyService.detele(id)
