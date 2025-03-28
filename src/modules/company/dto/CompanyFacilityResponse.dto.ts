4cabb09143804c3949c077d1450699447f5649fd
Daniel K Guolo <daniel_kv@hotmail.com>
2025-03-28 11:11:45 -0300

    Update CompanyFacilityResponseDTO to use FacilityResponseDTO for improved consistency in data representation

--- src/modules/company/dto/CompanyFacilityResponse.dto.ts
+++ src/modules/company/dto/CompanyFacilityResponse.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger'
 
-import { FacilityDTO } from '@/modules/facility/dto/Facility.dto'
+import { FacilityResponseDTO } from '@/modules/facility/dto/FacilityResponse.dto'
 
 export class CompanyFacilityResponseDTO {
     @ApiProperty({ example: 'bcd82497-2cc3-4998-b3d9-99db2f56b159' })
@@ -9,8 +9,8 @@ export class CompanyFacilityResponseDTO {
     @ApiProperty({ example: 1, nullable: true, type: Number })
     order: number | null
 
-    @ApiProperty({ type: FacilityDTO })
-    facility: FacilityDTO
+    @ApiProperty({ type: FacilityResponseDTO })
+    facility: FacilityResponseDTO
 
     @ApiProperty({ example: '2025-02-28T12:30:00.000Z' })
     createdAt!: Date
