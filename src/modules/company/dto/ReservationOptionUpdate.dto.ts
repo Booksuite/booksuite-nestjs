3fe1655d993d699c87b4c954e7d84099706a3d3f
Zaymã Kinsiona <127459308+Zayma1@users.noreply.github.com>
2025-04-03 15:24:45 -0300

    added required false to dto (#93)

--- src/modules/company/dto/ReservationOptionUpdate.dto.ts
+++ src/modules/company/dto/ReservationOptionUpdate.dto.ts
@@ -12,12 +12,16 @@ import { ReservationOptionAgeGroupDTO } from './ReservationOptionAgeGroup.dto'
 import { ReservationOptionHousingUnitTypeDTO } from './ReservationOptionHousingUnitType.dto'
 
 export class ReservationOptionUpdateDTO {
-    @ApiProperty({ example: 'Standard Reservation', type: String })
+    @ApiProperty({
+        example: 'Standard Reservation',
+        type: String,
+        required: false,
+    })
     @IsOptional()
     @IsString()
     name?: string
 
-    @ApiProperty({ example: true, type: Boolean })
+    @ApiProperty({ example: true, type: Boolean, required: false })
     @IsOptional()
     @IsDefined()
     published?: boolean
@@ -26,34 +30,47 @@ export class ReservationOptionUpdateDTO {
         enum: BillingType,
         enumName: 'BillingType',
         example: BillingType.DAILY,
+        required: false,
     })
     @IsOptional()
     @IsEnum(BillingType)
     billingType?: BillingType
 
-    @ApiProperty({ example: 20.5, type: Number })
+    @ApiProperty({ example: 20.5, type: Number, required: false })
     @IsOptional()
     @IsNumber()
     additionalAdultPrice?: number
 
-    @ApiProperty({ example: 10.0, type: Number })
+    @ApiProperty({ example: 10.0, type: Number, required: false })
     @IsOptional()
     @IsNumber()
     additionalChildrenPrice?: number
 
-    @ApiProperty({ example: '[0,1,2]', type: Number, isArray: true })
+    @ApiProperty({
+        example: '[0,1,2]',
+        type: Number,
+        isArray: true,
+        required: false,
+    })
     @IsOptional()
     availableWeekend?: PrismaJson.WeekDays
 
-    @ApiProperty({ type: [ReservationOptionHousingUnitTypeDTO] })
+    @ApiProperty({
+        type: [ReservationOptionHousingUnitTypeDTO],
+        required: false,
+    })
     @IsOptional()
     availableHousingUnitTypes?: ReservationOptionHousingUnitTypeDTO[]
 
-    @ApiProperty({ type: [ReservationOptionAgeGroupDTO], isArray: true })
+    @ApiProperty({
+        type: [ReservationOptionAgeGroupDTO],
+        isArray: true,
+        required: false,
+    })
     @IsOptional()
     ageGroupPrices?: ReservationOptionAgeGroupDTO[]
 
-    @ApiProperty({ type: [String] })
+    @ApiProperty({ type: [String], required: false })
     @IsOptional()
     includedItems?: PrismaJson.IncludedItems
 }
