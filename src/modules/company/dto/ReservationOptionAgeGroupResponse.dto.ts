0675626581b33418e4ec4fd676f39abf405b148a
Zaymã Kinsiona <127459308+Zayma1@users.noreply.github.com>
2025-04-04 16:05:46 -0300

    fix DTO (#98)

--- src/modules/company/dto/ReservationOptionAgeGroupResponse.dto.ts
+++ src/modules/company/dto/ReservationOptionAgeGroupResponse.dto.ts
@@ -11,7 +11,7 @@ export class ReservationOptionAgeGroupResponseDTO {
 
     @ApiProperty({
         example: '123e4567-e89b-12d3-a456-426614174000',
-        type: String,
+        type: AgeGroupDTO,
     })
     ageGroup: AgeGroupDTO
 
