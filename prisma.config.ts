51f5512fc074185696c57c3cd51e8aac5bc72325
Daniel K Guolo <daniel_kv@hotmail.com>
2025-04-09 11:15:20 -0300

    fix prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,9 +2,6 @@ import path from 'node:path'
 import { defineConfig } from 'prisma/config'
 
 export default defineConfig({
-    schema: {
-        kind: 'multi',
-        folderPath: path.join(__dirname, 'prisma', 'schema'),
-    },
     earlyAccess: true,
+    schema: path.join(__dirname, 'prisma', 'schema'),
 })
