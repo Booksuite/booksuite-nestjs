58ed0ac0930d46b23ce700f3ea2fbdba3d82646c
KevinGirelli <kevingirelli.gg@gmail.com>
2025-05-05 17:37:10 -0300

    Created Company Medias

---
+++ src/modules/company/dto/CompanyMediaUpdate.dto.ts
@@ -0,0 +1,23 @@
+import { ApiProperty } from '@nestjs/swagger'
+import { IsBoolean, IsInt, IsOptional, IsUUID } from 'class-validator'
+
+export class CompanyMediaUpdateDTO {
+    @ApiProperty({
+        example: '123e4567-e89b-12d3-a456-426614174000',
+        required: false,
+        type: String,
+    })
+    @IsOptional()
+    @IsUUID()
+    mediaId?: string
+
+    @ApiProperty({ example: true, required: false, type: Boolean })
+    @IsOptional()
+    @IsBoolean()
+    isFeatured?: boolean
+
+    @ApiProperty({ example: 1, required: false, nullable: true, type: Number })
+    @IsOptional()
+    @IsInt()
+    order?: number | null
+}
